5a2db6f23e215eb9f4d1263000812b06247e5683
Ubuntu <ubuntu@ip-172-31-6-107.us-east-2.compute.internal>
2021-06-01 21:31:56 +0000

    server

--- migrations/Version20210601191111.php
+++ migrations/Version20210601191111.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210528210534 extends AbstractMigration
+final class Version20210601191111 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,14 +20,12 @@ final class Version20210528210534 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE empresas ADD siglas VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE modulos CHANGE id id INT NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE empresas DROP siglas');
         $this->addSql('ALTER TABLE modulos CHANGE id id INT AUTO_INCREMENT NOT NULL');
     }
 }
