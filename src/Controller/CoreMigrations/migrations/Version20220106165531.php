374138d2b4984e39230d7e1ce38e7f6c15def0c0
leandrojc88 <leandrojc88@gmail.com>
2022-01-06 13:56:59 -0500

    migrations Version20220106165531

---
+++ src/Controller/CoreMigrations/migrations/Version20220106165531.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\CoreMigrations\migrations;
+
+use App\Controller\CoreMigrations\AbstratCoreMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220106165531 extends AbstratCoreMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up() : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE almacen ADD otro_destino TINYINT(1) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE almacen DROP otro_destino');
+    }
+}
