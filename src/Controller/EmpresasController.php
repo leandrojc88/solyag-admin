ec1a00e71a1daa446262e84f489cd9cef749d407
leandrojc88 <leandrojc88@gmail.com>
2022-01-15 14:02:52 -0500

    dd delete

--- src/Controller/EmpresasController.php
+++ src/Controller/EmpresasController.php
@@ -154,8 +154,6 @@ class EmpresasController extends AbstractController
                 ]
             );
 
-            dd($response);
-
             if ($response->getStatusCode() == 501)
                 $this->addFlash('error', 'el usuario esta en uso');
 
