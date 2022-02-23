017d9673b0a3538d87f7f82a93063bab27fe1043
leandrojc88 <leandrojc88@gmail.com>
2022-02-23 09:45:01 -0500

    migration

---
+++ src/Controller/CoreMigrations/migrations/Version20220223143746.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\CoreMigrations\migrations;
+
+use App\Controller\CoreMigrations\AbstratCoreMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220223143746 extends AbstratCoreMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up() : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE documento ADD nro_documento VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE mercancia ADD cantidad_transito DOUBLE PRECISION DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE documento DROP nro_documento');
+        $this->addSql('ALTER TABLE mercancia DROP cantidad_transito');
+    }
+}
