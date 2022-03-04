6528e4f2595a3ca6ba362ce7376c1e05f109cee7
leandrojc88 <leandrojc88@gmail.com>
2022-03-04 09:58:51 -0500

    migracion

---
+++ src/Controller/CoreMigrations/migrations/Version20220304145447.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\CoreMigrations\migrations;
+
+use App\Controller\CoreMigrations\AbstratCoreMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220304145447 extends AbstratCoreMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up() : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE movimiento_mercancia ADD aceptado TINYINT(1) DEFAULT NULL, ADD nota VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE movimiento_mercancia DROP aceptado, DROP nota');
+    }
+}
