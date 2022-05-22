d5d29aa8d234e2f86194c838b3b86070fe968cdb
leandrojc88 <leandrojc88@gmail.com>
2022-05-21 21:14:14 -0400

    migration

---
+++ src/Controller/CoreMigrations/migrations/Version20220522011147.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\CoreMigrations\migrations;
+
+use App\Controller\CoreMigrations\AbstratCoreMigration;
+use Doctrine\DBAL\Schema\Schema;
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220522011147 extends AbstratCoreMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up() : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE almacen ADD consignacion TINYINT(1) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE almacen DROP consignacion');
+    }
+}
