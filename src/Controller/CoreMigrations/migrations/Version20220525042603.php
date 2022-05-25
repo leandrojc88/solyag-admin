8b59b7e3071b322c0881b3f95b359ebd490e47e1
leandrojc88 <leandrojc88@gmail.com>
2022-05-25 09:44:45 -0400

    migracion

---
+++ src/Controller/CoreMigrations/migrations/Version20220525042603.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\CoreMigrations\migrations;
+
+use App\Controller\CoreMigrations\AbstratCoreMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220525042603 extends AbstratCoreMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up() : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE factura CHANGE fecha_factura fecha_factura DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE factura CHANGE fecha_factura fecha_factura DATE NOT NULL');
+    }
+}
