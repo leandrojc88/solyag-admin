216abc9156b8aaf003c744592fa81b129912576f
leandrojc88 <leandrojc88@gmail.com>
2022-06-07 14:30:08 -0400

    migracion status

---
+++ src/Controller/CoreMigrations/migrations/Version20220607182756.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\CoreMigrations\migrations;
+
+use App\Controller\CoreMigrations\AbstratCoreMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220607182756 extends AbstratCoreMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up() : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE movimiento_servicio ADD status VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE movimiento_servicio DROP status');
+    }
+}
