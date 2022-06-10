366306a4f0bba37ea7625bcf968b9d02c12ce3cf
leandrojc88 <leandrojc88@gmail.com>
2022-06-09 23:46:31 -0400

    eliminado tabla toma

--- migrations/Version20220609181224.php
+++ migrations/Version20220609181224.php
@@ -22,13 +22,11 @@ final class Version20220609181224 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE empresa_tipo_paga (id INT AUTO_INCREMENT NOT NULL, empresa_id INT NOT NULL, tipo VARCHAR(255) NOT NULL, saldo INT NOT NULL, UNIQUE INDEX UNIQ_15FE3703521E1991 (empresa_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE empresa_tipo_paga ADD CONSTRAINT FK_15FE3703521E1991 FOREIGN KEY (empresa_id) REFERENCES empresas (id)');
-        $this->addSql('DROP TABLE toma');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE toma (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('DROP TABLE empresa_tipo_paga');
     }
 }
