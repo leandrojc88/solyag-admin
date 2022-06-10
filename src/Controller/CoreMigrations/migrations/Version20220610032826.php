20a66c58845c241747089ebec7de58c83d8e01d7
leandrojc88 <leandrojc88@gmail.com>
2022-06-09 23:34:36 -0400

    migracion loteria

---
+++ src/Controller/CoreMigrations/migrations/Version20220610032826.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\CoreMigrations\migrations;
+
+use App\Controller\CoreMigrations\AbstratCoreMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220610032826 extends AbstratCoreMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up() : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE unidad ADD loteria TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE vale_salida_caja_chica ADD loteria TINYINT(1) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE unidad DROP loteria');
+        $this->addSql('ALTER TABLE vale_salida_caja_chica DROP loteria');
+    }
+}
