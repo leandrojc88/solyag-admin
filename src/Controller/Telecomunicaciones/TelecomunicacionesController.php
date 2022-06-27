d6ef38db8510bbd0f461e0b7cd2a302cf80f89ff
leandrojc88 <leandrojc88@gmail.com>
2022-06-27 16:29:38 -0400

    quitar la menu de reporte y que cage automaticamente en el dashboard de Telecomunicaicone

--- src/Controller/Telecomunicaciones/TelecomunicacionesController.php
+++ src/Controller/Telecomunicaciones/TelecomunicacionesController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\Telecomunicaciones;
 
+use App\Repository\Telecomunicaciones\ServicioEmpresaRepository;
+use App\Service\Telecomunicaciones\RecargaCubacel\ListRecargaCubacel;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,11 +15,44 @@ class TelecomunicacionesController extends AbstractController
     /**
      * @Route("/telecomunicaciones", name="telecomunicaciones")
      */
-    public function index(): Response
-    {
+    public function index(
+        Request $request,
+        PaginatorInterface $pagination,
+        ListRecargaCubacel $listRecargaCubacel,
+        ServicioEmpresaRepository $servicioEmpresaRepository
+    ): Response {
+        // listado
+        $list = ($listRecargaCubacel)([]);
+
+        $data = [];
+        foreach ($list as $recarga) {
+
+            $empresaServicio = $servicioEmpresaRepository->find($recarga["id"]);
+
+            $data[] = [
+                "id" => $recarga["id"],
+                "empresa" => $recarga["empresa"],
+                "descripcion" => $recarga["descripcion"],
+                "empleado" => $recarga["empleado"],
+                "no_orden" => $empresaServicio->noOrdeToStr(),
+                "no_telefono" => $recarga["no_telefono"],
+                "status" => $recarga["status"],
+                "date" => $recarga["date"],
+                "confirmation_date" => $recarga["confirmation_date"],
+                "servicio" => $recarga["servicio"],
+                "costo" => $recarga["costo"]
+            ];
+        }
+
+        $paginator = $pagination->paginate(
+            array_reverse($data),
+            $request->query->getInt('page', 1),
+            25,
+            ['align' => 'center', 'style' => 'bottom',]
+        );
 
         return $this->render('telecomunicaciones/index.html.twig', [
-            'controller_name' => 'TelecomunicacionesController',
+            "recargas" => $paginator
         ]);
     }
 }
