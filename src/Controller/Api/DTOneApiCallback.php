9581e989de842ff56fa40977f67fc146f904e8f2
leandrojc88 <leandrojc88@gmail.com>
2022-06-29 16:09:33 -0400

    `dtone_callback_url` terminada correctamente

--- src/Controller/Api/DTOneApiCallback.php
+++ src/Controller/Api/DTOneApiCallback.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Api;
 
 use App\Entity\Pais;
+use App\Service\DToneManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,22 +17,21 @@ class DTOneApiCallback extends AbstractController
     /**
      * @Route("/api/dtone-callback-url", name="dtone_callback_url", methods={"POST"})
      */
-    public function callback_url(Request $request): JsonResponse
-    {
-        // dd(json_decode($request->getContent(), true));
-        $em = $this->getDoctrine()->getManager();
+    public function callback_url(
+        Request $request,
+        DToneManager $dToneManager
+    ): JsonResponse {
 
-        // $result["status"]["message"],
+        $result = json_decode($request->getContent(), true);
 
-        $pais = new Pais();
-        $pais->setResponse(json_decode($request->getContent(), true));
-        $pais->setNombre('respuesta dtone');
-        $pais->setActivo(true);
-
-        $em->persist($pais);
-        $em->flush();
+        $dToneManager->updateDToneIntoServiceEmpresa(
+            $result["external_id"],
+            $result["id"],
+            $result["confirmation_date"],
+            $result["status"]["message"],
+            $result
+        );
 
         return $this->json('ok!');
     }
-
 }
