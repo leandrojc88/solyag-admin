f5a2dc37ecceee5b838fc65b16459a40b5b9fe5c
leandrojc88 <leandrojc88@gmail.com>
2022-06-29 10:24:43 -0400

    cambio de calback_url to `api/dtone-callback-url`

---
+++ src/Controller/Api/DTOneApiCallback.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Pais;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DTOneApiCallback extends AbstractController
+{
+
+
+    /**
+     * @Route("/api/dtone-callback-url", name="dtone_callback_url", methods={"POST"})
+     */
+    public function callback_url(Request $request): JsonResponse
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $pais = new Pais();
+        // $pais->setNombre(json_encode($request->request->all()));
+        $pais->setNombre('123');
+        $pais->setActivo(true);
+
+        $em->persist($pais);
+        $em->flush();
+
+        return $this->json('ok!');
+    }
+
+}
--- src/Controller/DToneController.php
+++ src/Controller/DToneController.php
@@ -2,27 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Pais;
-use App\Entity\Telecomunicaciones\EmpresaSubservicioCubacel;
 use App\Entity\Telecomunicaciones\ServicioEmpresa;
 use App\Http\httpPostServicioEmpresaCubacel;
-use App\Repository\Telecomunicaciones\EmpresaSubservicioCubacelRepository;
 use App\Repository\Telecomunicaciones\ServicioEmpresaRepository;
 use App\Service\DToneManager;
 use App\Service\Telecomunicaciones\Config\GetLoadIsActiveService;
-use App\Service\Telecomunicaciones\Empresas\EmpresaTipoPagoService;
 use App\Service\Telecomunicaciones\Empresas\ServicioEmpresaService;
 use App\Service\Telecomunicaciones\Empresas\ValidateSaldoEmpresa;
-use DateTime;
-use Doctrine\ORM\EntityManagerInterface;
-use Exception;
 use App\Types\Status;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\Json;
 
 /**
  * Class DToneController
@@ -39,8 +29,7 @@ class DToneController extends AbstractController
         DToneManager $dToneManager,
         ServicioEmpresaService $servicioEmpresaService,
         httpPostServicioEmpresaCubacel $httpPostServicioEmpresaCubacel,
-        GetLoadIsActiveService $getLoadIsActiveService,
-        ValidateSaldoEmpresa $validateSaldoEmpresa
+        GetLoadIsActiveService $getLoadIsActiveService
     ): JsonResponse {
 
         if (!$getLoadIsActiveService->get())
@@ -82,22 +71,4 @@ class DToneController extends AbstractController
         return $this->json(["finish" => true]);
     }
 
-
-    /**
-     * @Route("/callback_url", name="callback_url", methods={"POST"})
-     */
-    public function callback_url(): JsonResponse
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $pais = new Pais();
-        // $pais->setNombre(json_encode($request->request->all()));
-        $pais->setNombre('123');
-        $pais->setActivo(true);
-
-        $em->persist($pais);
-        $em->flush();
-
-        return $this->json('ok!');
-    }
 }
