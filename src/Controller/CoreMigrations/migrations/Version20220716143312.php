4660acb9b9f8b5da70533a7f9746956ad333fb92
leandrojc88 <leandrojc88@gmail.com>
2022-07-16 10:34:15 -0400

    migration

---
+++ src/Controller/CoreMigrations/migrations/Version20220716143312.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\CoreMigrations\migrations;
+
+use App\Controller\CoreMigrations\AbstratCoreMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220716143312 extends AbstratCoreMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up() : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE movimiento_servicio ADD json LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE movimiento_servicio DROP json');
+    }
+}
