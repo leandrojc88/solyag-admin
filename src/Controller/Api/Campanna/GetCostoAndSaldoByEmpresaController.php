bf6b68902382d2b1b39a0655915d0376357e1689
leandrojc88 <leandrojc88@gmail.com>
2022-07-25 17:09:07 -0400

    api para costo y saldo

---
+++ src/Controller/Api/Campanna/GetCostoAndSaldoByEmpresaController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Api\Campanna;
+
+use App\Repository\Campanna\CampannaConfigRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GetCostoAndSaldoByEmpresaController extends AbstractController
+{
+
+    /**
+     * @Route("/api/campanna/get-costo-and-saldo-by-empresa")
+     */
+    public function index(
+        Request $request,
+        CampannaConfigRepository $campannaConfigRepository
+    ): JsonResponse {
+
+        $id_empresa = $request->get('id_empresa');
+
+        $campannaConfig = $campannaConfigRepository->findOneBy(['empresa' => $id_empresa]);
+
+        return $this->json([
+            'costo' => $campannaConfig->getCosto(),
+            'saldo' => $campannaConfig->getSaldo()
+        ]);
+    }
+}
